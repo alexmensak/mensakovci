f20b74db5153340712153332293794b040fc6c33
alexmensak <alex@optondemand.com>
2026-04-26 18:18:32 +0200

    Nav fix

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -3,6 +3,6 @@ export type NavLink = { label: string; to: string };
 export const navLinks: NavLink[] = [
   { label: 'Miesto', to: '/#miesto' },
   { label: 'Harmonogram', to: '/#harmonogram' },
-  { label: 'Oblečenie', to: '/#oblecenie' },
+  { label: 'Dresscode', to: '/#oblecenie' },
   { label: 'Dary', to: '/#dary' },
 ];
